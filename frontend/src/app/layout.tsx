9e7583841496ad02ea51a93b53942976b3e11ba5
Marc Tschebull <marc.tschebull@sap.com>
2025-02-25 20:01:03 +0100

    modify login

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,12 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import "@mantine/core/styles.css";
+import {
+    ColorSchemeScript,
+    MantineProvider,
+} from "@mantine/core";
+import { ThemeProvider } from "@/components/theme-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +33,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+      <ThemeProvider>
+          <MantineProvider>{children}</MantineProvider>
+      </ThemeProvider>
       </body>
     </html>
   );
